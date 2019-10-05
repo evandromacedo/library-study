4b659237ef6de7b6e2750ea70c413ec7e9b7ea14
Evandro Macedo <evandro.hcm@gmail.com>
2019-10-05 00:45:24 -0300

    Moves react and react-dom to peerDependencies

--- webpack.config.js
+++ webpack.config.js
@@ -37,11 +37,19 @@ module.exports = {
         ]
       }
     ]
+  },
+  externals: {
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "react",
+      root: "React"
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "react-dom",
+      root: "ReactDOM"
+    }
   }
-  // plugins: [
-  //   new HtmlWebPackPlugin({
-  //     template: "./src/index.html",
-  //     filename: "./index.html"
-  //   })
-  // ]
 };
